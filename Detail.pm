ad795797e5fc3daf7af6b91e1f1ffbbc71c20055
- <stefanos@cpan.org>
2013-03-03 01:31:22 +0000

    Import of STEFANOS/WWW-UPS-Detail-0.4 from CPAN.
    gitpan-cpan-distribution: WWW-UPS-Detail
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         STEFANOS/WWW-UPS-Detail-0.4.tar.gz
    gitpan-cpan-author:       STEFANOS
    gitpan-cpan-maturity:     released

--- Detail.pm
+++ Detail.pm
@@ -4,7 +4,7 @@ use strict;
 use Exporter;
 our @ISA = qw/Exporter/;
 our @EXPORT = qw/upscheck/;
-our $VERSION = '0.3';
+our $VERSION = '0.4';
 use LWP::Simple;
 use LWP::UserAgent;
 
@@ -173,12 +173,9 @@ WWW::UPS::Detail - Perl module for the UPS online tracking service with details.
 
 =head1 AUTHOR
 
-    Stefan Gipper <stefanos [AT] cpan [DOT] org>
+    -
 
 =head1 COPYRIGHT
-
-	WWW::UPS::Detail is Copyright (c) 2012 Stefan Gipper
-	All rights reserved.
 
 	This program is free software; you can redistribute
 	it and/or modify it under the same terms as Perl itself.
